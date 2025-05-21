9d74d1038acb07ace708ea67a2db779b81968ca9
yin. <168466926+lavynayien@users.noreply.github.com>
2025-05-21 18:23:24 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U KAK FIRA').split('')
+    const titles = ('I LOVE U').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
